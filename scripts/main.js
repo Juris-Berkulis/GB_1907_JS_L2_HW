3dc7ee3f47073c21ce8e92077746e0ef05c3813a
Juris-Berkulis <krztfjrbhm1@jmail.com>
2021-07-21 23:06:50 +0300

    1. Add the file "index.html" with the initial html-code; 2. Add the "main.js" file with the initial js-code to the "scripts" folder; 3. Improve html markup; 4. Add a title for the list of products; 5. Add 4 pictures of goods to the "img" folder; 6. Connect product pictures using js-code; 7. Add the "style.scss" styles file to the "styles" folder, connect it to the "index.html" file and generate the "style.css" file; 8. Add styles for all content; 9. Improve the js-code.

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+let divProductsList = document.querySelector('.products_list');
+
+const products = [
+    { id: 1, title: 'Notebook', price: 2000 },
+    { id: 2, title: 'Mouse', price: 20 },
+    { id: 3, title: 'Keyboard', price: 200 },
+    { id: 4, title: 'Gamepad', price: 50 },
+];
+
+//Функция для формирования верстки каждого товара
+//Добавить в выводе изображение
+const renderProduct = (item) => {
+    return `<div class="product_item">
+                <img class="product_img" src="img/${item.title}.jpg" alt="${item.title}" width="200">
+                <h3 class="product_title">${item.title}</h3>
+                <p class="product_description">${item.price}</p>
+                <button class="product_buy_btn">Купить</button>
+            </div>`
+};
+
+const renderPage = (list = products) => {
+    const productsList = list.map(item => renderProduct(item));
+    console.log(productsList);
+    for (let i = 1; i <= productsList.length; i++) {
+        divProductsList.innerHTML += productsList[i - 1];
+    }
+};
+
+renderPage();
