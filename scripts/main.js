a3f99dd72cdc931b9452fd9e3c3d51fb01e23158
Juris-Berkulis <krztfjrbhm1@jmail.com>
2021-07-29 00:46:08 +0300

    1. Add a classes for displaying a list of products in the cart; 2. Add add styles for the list of products in the shopping cart; 3. Add a button to close the list of basket items; 4. Add the functions of opening and closing the list of products in the basket; 5. Add minor improvements.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,21 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
+const btnBasket = document.querySelector('.btn_cart');
+btnBasket.onclick = () => {
+    const btnBasketClose = document.querySelector('.basket_close');
+    btnBasketClose.style.display = 'block';
+    const divBasketList = document.querySelector('.basket_list');
+    divBasketList.style.display = 'block';
+};
+
+const btnBasketClose = document.querySelector('.basket_close');
+btnBasketClose.onclick = () => {
+    const btnBasketClose = document.querySelector('.basket_close');
+    btnBasketClose.style.display = 'none';
+    const divBasketList = document.querySelector('.basket_list');
+    divBasketList.style.display = 'none';
+};
+
 class ProductList{
     constructor(container='.products_list'){
         this.container = container;
@@ -8,7 +24,7 @@ class ProductList{
             .then(data => { //data - объект js
                 this.goods = [...data];
                 this.render(); // вывод товаров на страницу.
-                this.getSum();
+                // this.getSum();
             });
     }
 
@@ -28,13 +44,13 @@ class ProductList{
         }
     }
 
-    getSum() {
-        let sum = 0;
-        this.goods.forEach(item => {
-            sum += item.price;
-        })
-        document.querySelector('.products').insertAdjacentHTML("beforeend", `Сумма товаров: ${sum} руб.`);
-    }
+    // getSum() {
+    //     let sum = 0;
+    //     this.goods.forEach(item => {
+    //         sum += item.price;
+    //     })
+    //     document.querySelector('.products').insertAdjacentHTML("beforeend", `Сумма товаров: ${sum} руб.`);
+    // }
 }
 
 class ProductItem{
@@ -57,27 +73,80 @@ class ProductItem{
 }
 
 class Basket {
-    addProduct() {
-        //! TODO: заглушка.
+    constructor(container='.basket_list'){
+        this.container = container;
+        this.basket_products = [];
+        this._fetchBasketProducts()
+            .then(data => { //data - объект js
+                this.basket_products = [...data.contents];
+                this.render(); // вывод товаров корзины на страницу.
+            });
     }
 
-    removeProduct() {
-        //! TODO: заглушка.
+    _fetchBasketProducts() {
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
     }
-
-    changeProduct() {
-        //! TODO: заглушка.
-    }
-
+    
     render() {
-        //! TODO: заглушка.
+        const block = document.querySelector(this.container);
+        for(let product of this.basket_products){
+            const item = new itemInBasket(product);
+            block.insertAdjacentHTML("beforeend", item.render()); // Аналог для block.innerHTML += item.render();, но работает быстрее.
+        }
     }
+
+    // addProduct() {
+    //     //! TODO: заглушка.
+    // }
+
+    // removeProduct() {
+    //     //! TODO: заглушка.
+    // }
+
+    // changeProduct() {
+    //     //! TODO: заглушка.
+    // }
+
+    // render() {
+    //     //! TODO: заглушка.
+    // }
 }
 
 class itemInBasket {
-    render() {
-        //! TODO: заглушка.
+    constructor(product) {
+        this.title = product.product_name;
+        this.id = product.id_product;
+        this.price = product.price;
+        this.imgSrc = `img/${product.product_name}.jpg`;
+        this.imgAlt = `${product.product_name}`;
+        this.imgWidth = "200";
+        this.count = product.quantity;
     }
+    render() {
+        return `<div class="basket_item">
+                    <div class="basket_left">
+                        <img class="basket_img" src="${this.imgSrc}" alt="${this.imgAlt}" width="${this.imgWidth}">
+                        <h3 class="basket_title">${this.title}</h3>
+                    </div>
+                    <div class="basket_right">
+                        <p class="basket_description">${this.price}</p>
+                        <div class="basket_panel">
+                            <button class="basket_btn basket_btn-minus">-</button>
+                            <p class="basket_count">${this.count}</p>
+                            <button class="basket_btn basket_btn-plus">+</button>
+                        </div>
+                    </div>
+                </div>`
+    }
+
+    // render() {
+    //     //! TODO: заглушка.
+    // }
 }
 
 let list = new ProductList();
+let listBasket = new Basket();
