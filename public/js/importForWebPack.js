13571aea8202032117398f04f931e78c4f6431d4
Juris-Berkulis <krztfjrbhm1@jmail.com>
2021-08-21 05:29:21 +0300

    1. Generate js-code using WebPack; 2. Include in the file "index.html" instead of five js-files one js-file generated using WebPack.

---
+++ public/js/importForWebPack.js
@@ -0,0 +1,13 @@
+import CartComponent from './CartComponent';
+import ErrorComp from './ErrorComp';
+import FilterComp from './FilterComp';
+import ProductComponent from './ProductComponent';
+import main from './main';
+
+export default {
+    components: CartComponent,
+    ErrorComp,
+    FilterComp,
+    ProductComponent,
+    main,
+}
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    entry: './public/js/importForWebPack.js',
+    output: {
+        path: 'C:\/lessons\/GB\/Courses\/Profession_Web_Developer\/04_JavaScript_Level_2\/public\/js',
+        filename: './codeFromWebPack.js'
+    },
+    watch: true,
+}
